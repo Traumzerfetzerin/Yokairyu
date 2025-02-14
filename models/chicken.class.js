e69631c58fbe585fc5cc3a1aae0f55008c6a5eab
Traumzerfetzerin <168225733+Traumzerfetzerin@users.noreply.github.com>
2025-02-15 00:19:53 +0100

    add animation

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -8,5 +8,12 @@ class Chicken extends MovableObject {
         super().loadImage('../img/enemy/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
 
         this.x = 200 + Math.random() * 500; // Zahl wischen 200 und 700
+        this.animate();
+    }
+
+    animate() {
+        setInterval(() => {
+            this.x -= 0.2;
+        }, 1000 / 60);
     }
 }
--- models/cloud.class.js
+++ models/cloud.class.js
@@ -1,12 +1,19 @@
 class Cloud extends MovableObject {
 
     y = 10;
-    width = 300;
-    height = 150;
+    width = 500;
+    height = 300;
 
 
     constructor() {
         super().loadImage('../img/background/4_clouds/1.png');
         this.x = Math.random() * 500;
+        this.animate();
+    }
+
+    animate() {
+        setInterval(() => {
+            this.x -= 0.2;
+        }, 1000 / 60);
     }
 }
