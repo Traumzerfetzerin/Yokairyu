1b561fb7f867545da03027743b4ae367544dde14
Traumzerfetzerin <168225733+Traumzerfetzerin@users.noreply.github.com>
2025-03-11 22:01:47 +0100

    optimizing

--- levels/level1.js
+++ levels/level1.js
@@ -1,62 +1,58 @@
-let level1;
-function initLevel1() {
+const level1 = new Level(
+    [
+        // new Chicken(),
+        // new Chicken(),
+        new Chicken(),
+        new Endboss(),
+    ],
+    [
+        new Cloud(),
+        new Cloud(),
+        new Cloud()
 
-    level1 = new Level(
-        [
-            // new Chicken(),
-            // new Chicken(),
-            new Chicken(),
-            new Endboss(),
-        ],
-        [
-            new Cloud(),
-            new Cloud(),
-            new Cloud()
+    ],
+    [
+        // new Coins(),
+        // new Coins(),
+        // new Coins(),
+        // new Coins(),
+        // new Coins(),
+        // new Coins()
+    ],
+    [
+        // new Bottles(),
+        // new Bottles(),
+        // new Bottles(),
+        // new Bottles(),
+        // new Bottles(),
+        // new Bottles()
+    ],
 
-        ],
-        [
-            // new Coins(),
-            // new Coins(),
-            // new Coins(),
-            // new Coins(),
-            // new Coins(),
-            // new Coins()
-        ],
-        [
-            // new Bottles(),
-            // new Bottles(),
-            // new Bottles(),
-            // new Bottles(),
-            // new Bottles(),
-            // new Bottles()
-        ],
+    [
+        new BackgroundObject('./img/background/Plan-5.png', -720),
+        new BackgroundObject('./img/background/Plan-3.png', -720),
+        new BackgroundObject('./img/background/Plan-2.png', -720),
+        new BackgroundObject('./img/background/Plan-1.png', -720),
 
-        [
-            new BackgroundObject('./img/background/Plan-5.png', -720),
-            new BackgroundObject('./img/background/Plan-3.png', -720),
-            new BackgroundObject('./img/background/Plan-2.png', -720),
-            new BackgroundObject('./img/background/Plan-1.png', -720),
+        new BackgroundObject('./img/background/Plan-5.png', 0),
+        // new BackgroundObject('./img/background/Plan-4.png', 0),
+        new BackgroundObject('./img/background/Plan-3.png', 0),
+        new BackgroundObject('./img/background/Plan-2.png', 0),
+        new BackgroundObject('./img/background/Plan-1.png', 0),
 
-            new BackgroundObject('./img/background/Plan-5.png', 0),
-            // new BackgroundObject('./img/background/Plan-4.png', 0),
-            new BackgroundObject('./img/background/Plan-3.png', 0),
-            new BackgroundObject('./img/background/Plan-2.png', 0),
-            new BackgroundObject('./img/background/Plan-1.png', 0),
+        new BackgroundObject('./img/background/Plan-5.png', 720),
+        new BackgroundObject('./img/background/Plan-3.png', 720),
+        new BackgroundObject('./img/background/Plan-2.png', 720),
+        new BackgroundObject('./img/background/Plan-1.png', 720),
 
-            new BackgroundObject('./img/background/Plan-5.png', 720),
-            new BackgroundObject('./img/background/Plan-3.png', 720),
-            new BackgroundObject('./img/background/Plan-2.png', 720),
-            new BackgroundObject('./img/background/Plan-1.png', 720),
+        new BackgroundObject('./img/background/Plan-5.png', 720 * 2),
+        new BackgroundObject('./img/background/Plan-3.png', 720 * 2),
+        new BackgroundObject('./img/background/Plan-2.png', 720 * 2),
+        new BackgroundObject('./img/background/Plan-1.png', 720 * 2),
 
-            new BackgroundObject('./img/background/Plan-5.png', 720 * 2),
-            new BackgroundObject('./img/background/Plan-3.png', 720 * 2),
-            new BackgroundObject('./img/background/Plan-2.png', 720 * 2),
-            new BackgroundObject('./img/background/Plan-1.png', 720 * 2),
-
-            new BackgroundObject('./img/background/Plan-5.png', 720 * 3),
-            new BackgroundObject('./img/background/Plan-3.png', 720 * 3),
-            new BackgroundObject('./img/background/Plan-2.png', 720 * 3),
-            new BackgroundObject('./img/background/Plan-1.png', 720 * 3),
-        ],
-    );
-}
+        new BackgroundObject('./img/background/Plan-5.png', 720 * 3),
+        new BackgroundObject('./img/background/Plan-3.png', 720 * 3),
+        new BackgroundObject('./img/background/Plan-2.png', 720 * 3),
+        new BackgroundObject('./img/background/Plan-1.png', 720 * 3),
+    ],
+);
