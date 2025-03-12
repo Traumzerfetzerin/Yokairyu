b07241e4d10d67168db8df9383d3901be5853756
Traumzerfetzerin <168225733+Traumzerfetzerin@users.noreply.github.com>
2025-03-12 20:12:32 +0100

    optimizing statusbar endboss

--- models/statusbarEndboss.class.js
+++ models/statusbarEndboss.class.js
@@ -1,10 +1,19 @@
 class StatusbarEndboss extends DrawableObject {
     IMAGES = [
-        './img/7_statusbars/2_statusbar_endboss/green.png'
+        './img/7_statusbars/2_statusbar_endboss/0.png',
+        './img/7_statusbars/2_statusbar_endboss/20.png',
+        './img/7_statusbars/2_statusbar_endboss/40.png',
+        './img/7_statusbars/2_statusbar_endboss/60.png',
+        './img/7_statusbars/2_statusbar_endboss/80.png',
+        './img/7_statusbars/2_statusbar_endboss/blue.png',
     ];
 
 
-
+    /**
+     * Initializes a new instance of the StatusbarEndboss class.
+     * Loads images for the status bar and sets its initial position
+     * and dimensions. The percentage is initially set to 100.
+     */
     constructor() {
         super();
         this.loadImages(this.IMAGES);
@@ -12,6 +21,6 @@ class StatusbarEndboss extends DrawableObject {
         this.y = 50;
         this.width = 200;
         this.height = 60;
-        this.setPercentage(0);
+        this.setPercentage(100);
     }
 }
