4aaab7dfcd1152bc6c765e8a6be3f092a447fcfe
Traumzerfetzerin <168225733+Traumzerfetzerin@users.noreply.github.com>
2025-04-24 20:42:10 +0200

    optimizing fullscreen

--- js/game.js
+++ js/game.js
@@ -79,7 +79,7 @@ function startGame() {
 }
 
 
-window.matchMedia('(pointer: coarse)').addEventListener('change', updateTouchButtonVisibility);
+pointerQuery.addEventListener('change', updateTouchButtonVisibility);
 
 
 /**
@@ -124,23 +124,63 @@ function toggleDropdown() {
 
 
 /**
- * Toggles the game canvas to full screen mode.
- * If the canvas is currently in windowed mode, it is set to full screen mode.
- * If the canvas is currently in full screen mode, it is set to windowed mode.
- * The function uses the appropriate full screen mode API depending on the
- * browser vendor (e.g. requestFullscreen for Chrome and Firefox, webkitRequestFullscreen
- * for Safari, and msRequestFullscreen for IE11).
+ * Requests the browser to display the specified element in fullscreen mode.
+ * 
+ * This function attempts to switch the given element to fullscreen by calling
+ * the appropriate method for the current browser. It first checks for the modern
+ * `requestFullscreen` method, then checks for vendor-prefixed implementations
+ * for WebKit and Internet Explorer.
+ * 
+ * @param {HTMLElement} element - The DOM element to be displayed in fullscreen.
+ */
+function enterFullscreen(element) {
+    if (element.requestFullscreen) {
+        element.requestFullscreen();
+    } else if (element.webkitRequestFullscreen) {
+        element.webkitRequestFullscreen();
+    } else if (element.msRequestFullscreen) {
+        element.msRequestFullscreen();
+    }
+}
+
+
+/**
+ * Exits fullscreen mode by calling the appropriate method for the current
+ * browser. This method first checks for the modern `exitFullscreen` method,
+ * then checks for vendor-prefixed implementations for WebKit and Internet
+ * Explorer.
+ */
+function exitFullscreen() {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) {
+        document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) {
+        document.msExitFullscreen();
+    }
+}
+
+
+/**
+ * Toggles the fullscreen mode for the game container element.
+ *
+ * If the game is not currently in fullscreen mode, this function requests
+ * fullscreen for the game container. If the game is already in fullscreen
+ * mode, it exits fullscreen. The function checks for support of the modern
+ * `fullscreenElement` property and its vendor-prefixed implementations.
  */
 function toggleFullscreen() {
+    const container = document.getElementById('game');
 
-    let container = document.getElementById('game');
+    const isFullscreen =
+        document.fullscreenElement ||
+        document.webkitFullscreenElement ||
+        document.msFullscreenElement;
 
-    if (container.requestFullscreen) {
-        container.requestFullscreen();
-    } else if (container.webkitRequestFullscreen) { /* Safari */
-        container.webkitRequestFullscreen();
-    } else if (container.msRequestFullscreen) { /* IE11 */
-        container.msRequestFullscreen();
+    if (!isFullscreen) {
+        enterFullscreen(container);
+    } else {
+        exitFullscreen();
     }
 }
 
